868ce687aae2f7f935605dde8a0b5b6a343a462e
Sujit Baniya <spbaniya@deerwalk.com>
2016-10-21 14:28:52 +0545

    Logo upadte

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
         <li class="sidebar-brand">
             <a href="{{ url('/') }}">
                 <img src="{{ url('/logo.png') }}" alt="United Gold Mart" style="width: 50px !important;">
-                United Gold Mart
+                The United Gold Mart
             </a>
         </li>
        @include('layouts.menu')

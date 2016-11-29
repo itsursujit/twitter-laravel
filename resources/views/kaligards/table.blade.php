8ac59c1e7ab57fdd24d15b680c555f061ac15b17
Sujit Baniya <spbaniya@deerwalk.com>
2016-11-29 14:48:48 +0545

    Tree vew

--- resources/views/kaligards/table.blade.php
+++ resources/views/kaligards/table.blade.php
@@ -6,7 +6,8 @@
             <th>Name</th>
             <th>Nationality</th>
             <th>Address</th>
-            <th>Id Card Type</th>
+            <th>Phone 1</th>
+            <th>Phone 2</th>
             <th>Notes</th>
             <th colspan="3">Action</th>
         </tr>
@@ -23,7 +24,8 @@
             <td>{!! $kaligard->first_name . ' ' . $kaligard->middle_name . ' ' . $kaligard->last_name !!}</td>
             <td>{!! $kaligard->nationality !!}</td>
             <td>{!! $kaligard->address !!}</td>
-            <td>{!! $kaligard->id_card_type !!}</td>
+            <td>{!! $kaligard->phone_1 !!}</td>
+            <td>{!! $kaligard->phone_2 !!}</td>
             <td>{!! $kaligard->notes !!}</td>
             <td>
                 {!! Form::open(['route' => ['kaligards.destroy', $kaligard->id], 'method' => 'delete']) !!}
